e73abdd97fc9071528d13c93b2d85d8775a33bda
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-10-28 13:59:40 +0200

    create footer, header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hy</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Hy</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   )
 }
 
