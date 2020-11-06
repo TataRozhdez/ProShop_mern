c30f7b0eaf64b7f2dc2060dc6cfc4eec85b2fc5e
Tata Rozhdez <tanya.zhukova.95@gmail.com>
2020-11-06 10:31:29 +0200

    user login reducer

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import {
   authUser,
-  // registerUser,
+  registerUser,
   getUserProfile,
   // updateUserProfile,
   // getUsers,
@@ -13,7 +13,7 @@ import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-// router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 //   .put(protect, updateUserProfile)
